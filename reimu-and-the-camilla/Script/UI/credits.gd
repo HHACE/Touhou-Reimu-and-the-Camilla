68230f83a067473b491f40b6f588276ce30572bc
HuyHACE <huynhhuyhuy12@gmail.com>
2024-10-27 23:36:41 +0700

    merge fix
    merge fix

--- reimu-and-the-camilla/Script/UI/credits.gd
+++ reimu-and-the-camilla/Script/UI/credits.gd
@@ -4,10 +4,7 @@ func _process(delta: float) -> void:
 	if !visible:
 		
 		return;
-	else:
-		$"../Logo".visible = false
 	if (Input.is_action_just_pressed("cancel") || Input.is_action_just_pressed("confirm")):
 		visible = false;
-		$"../Logo".visible = true
 		await get_tree().process_frame
 		get_parent().ReturnToMenu();
