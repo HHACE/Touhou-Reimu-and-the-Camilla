ed7ff301eb918a0117d17a2720abc635fcf97e21
Pioziomgames <73062121+Pioziomgames@users.noreply.github.com>
2024-10-27 15:12:48 +0100

    Game Over and Victory Screen

--- reimu-and-the-camilla/Script/victory.gd
+++ reimu-and-the-camilla/Script/victory.gd
@@ -1,14 +1,11 @@
 extends Panel
 
-var tryAgainSelected = true;
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_set_all_process(false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	_menu_control()
 	_submit_selected()
 
 func _set_all_process(x : bool):
@@ -18,17 +15,6 @@ func _set_all_process(x : bool):
 		$".".visible = true
 	else:
 		$".".visible = false
-
-func _menu_control():
-	var moved = false
-	if Input.is_action_just_pressed("move_left") || Input.is_action_just_pressed("move_right"):
-		tryAgainSelected = !tryAgainSelected;
-		if tryAgainSelected:
-			$TryAgain/Selection.show();
-			$MainMenu/Selection.hide();
-		else:
-			$MainMenu/Selection.show();
-			$TryAgain/Selection.hide();
 			
 func _submit_selected():
 
@@ -41,8 +27,4 @@ func _submit_selected():
 			if child.has_method("reset_pool"):
 				child.reset_pool()
 		GameManager.reset_game()
-		
-		if tryAgainSelected:
-			get_tree().change_scene_to_file("res://Scene/game_prototype.tscn")
-		else:
-			get_tree().change_scene_to_file("res://Scene/MainMenu.tscn")
+		get_tree().change_scene_to_file("res://Scene/MainMenu.tscn")
